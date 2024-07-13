0b94f6a754f02fec934586a4d6ebe8579bf9c860
Radicat <2285225334@qq.com>
2024-07-13 09:36:04 +0800

    retry setuptools_scm -24.7.13

--- src/toolagent/__init__.py
+++ src/toolagent/__init__.py
@@ -1,4 +1,8 @@
-from ._version import __version__
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0.0.0"
+
 from .utils._log import setup_logging as _setup_logging
 
 logger = _setup_logging()
